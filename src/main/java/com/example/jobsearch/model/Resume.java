94c29280384f966acdbd9448127a071b0b613e84
Egor Kirin <egor.kirin20@gmail.com>
2024-03-07 00:01:11 +0600

    added Resume and Category models

---
+++ src/main/java/com/example/jobsearch/model/Resume.java
@@ -0,0 +1,23 @@
+package com.example.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resume {
+    private Integer id;
+    private Integer applicant_id;
+    private String name;
+    private Integer category_id;
+    private Float salary;
+    private Boolean is_active;
+    private LocalDateTime created_date;
+    private LocalDateTime update_time;
+}
