b9e441add7601259db597b8d6f6b639a3d3ef944
Egor Kirin <egor.kirin20@gmail.com>
2024-03-14 23:43:43 +0600

    completed task1 with migrations and fixed mistakes from previous homework

--- src/main/java/com/example/jobsearch/dao/UserDao.java
+++ src/main/java/com/example/jobsearch/dao/UserDao.java
@@ -3,7 +3,6 @@ package com.example.jobsearch.dao;
 import com.example.jobsearch.dto.UserDto;
 import com.example.jobsearch.model.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -77,16 +76,16 @@ public class UserDao {
 
     public Boolean isUserInSystem(String email) {
         String sql = """
-                select * from users
-                where EMAIL = ?;
+                select case
+                when exists(select *
+                            from USERS
+                            where EMAIL = ?)
+                    then true
+                else false
+                end;
                 """;
 
-        try {
-            User user = template.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), email);
-            return user != null;
-        } catch (EmptyResultDataAccessException e) {
-            return false;
-        }
+        return template.queryForObject(sql, new BeanPropertyRowMapper<>(Boolean.class), email);
     }
 
 
